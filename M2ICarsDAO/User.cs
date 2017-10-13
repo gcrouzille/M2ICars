c2937c03a7f8278de4a4f8bddaf683cffeb29d8c
Unknown <boufflers.nicolas@gmail.com>
2017-10-13 15:03:43 +0200

    Entité User

--- M2ICarsDAO/User.cs
+++ M2ICarsDAO/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,24 @@ namespace M2ICarsDAO
 {
     public class User
     {
+        [Key]
+        public int ClientId { get; set; }              
+        public string Firstname { get; set; }        
+        public string Lastname { get; set; }                  
+        public DateTime Birthday { get; set; }        
+        public string Phone { get; set; }
+        public int Gender { get; set; }              
+        public string Email { get; set; }
+        public string PhotoUrl { get; set; }
+        public string Password { get; set; }
+       
+        public virtual Driver Driver { get;set; }
+        [ForeignKey("Driver")]
+        public int DriverId { get; set; }
 
+        public User()
+        {
+
+        }
     }
 }
