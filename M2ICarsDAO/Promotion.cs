b5c17f1bf5db52b2f87167d3ba856d9f03f39edb
Unknown <boufflers.nicolas@gmail.com>
2017-10-13 15:49:55 +0200

    Entité Promotion

--- M2ICarsDAO/Promotion.cs
+++ M2ICarsDAO/Promotion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace M2ICarsDAO
 {
     public class Promotion
     {
+        [Key]
+        public int PromotionId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Reduction { get; set; }
+
+        public Promotion()
+        {
+
+        }        
     }
 }
