51e43862dc249dfc11869182539d5dffd471bbc4
Unknown <ClarisseBaptiste90@gmail.com>
2017-10-13 15:02:50 +0200

    Entités driver + admin

--- M2ICarsDAO/Admin.cs
+++ M2ICarsDAO/Admin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace M2ICarsDAO
 {
     public class Admin
     {
+        [Key]
+        public int AdminId { get; set; }
+
+        public string Mail { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string Password { get; set; }
+
+        public int Access { get; set; }
     }
 }
--- M2ICarsDAO/Driver.cs
+++ M2ICarsDAO/Driver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,18 @@ namespace M2ICarsDAO
 {
     public class Driver
     {
+        [Key]
+        public int DriverId { get; set;}
+
+        public string RegisterState { get; set; }
+
+        public string Availability { get; set; }
+
+        public string CarBrand { get; set; }
+
+        public string CarType { get; set; }
+
+        public string Immatriculation { get; set; }
+
     }
 }
