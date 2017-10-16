598d9bbb9e1b77845a056dd26f04b63f1f92f134
Unknown <guillaumecrouzille@gmail.com>
2017-10-16 09:11:25 +0200

    Ajout des controlers de l'API

---
+++ M2ICarsAPI/Controllers/StatsController.cs
@@ -0,0 +1,17 @@
+﻿using M2ICarsDAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace M2ICarsAPI.Controllers
+{
+    public class StatsController : ApiController
+    {
+        private DB db = new DB();
+
+
+    }
+}
