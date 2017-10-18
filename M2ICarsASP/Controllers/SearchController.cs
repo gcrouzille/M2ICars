317d1c39b61e12b87a00375d43d3743731fc0a90
Unknown <guillaumecrouzille@gmail.com>
2017-10-18 09:59:40 +0200

    ASP - recherche de trajet V1

---
+++ M2ICarsASP/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M2ICarsASP.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Search
+        [ValidateAntiForgeryToken]
+        public string Search(string departure, string arrival)
+        {
+            return $"Demande de chauffeur pour un trajet de {departure} jusque {arrival}";
+        }
+    }
+}
